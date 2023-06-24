1e8f7010252699938b3be3b01e137b4b654958ab
Ryotruuser <ryonantruuser@gmail.com>
2023-06-24 12:00:15 -0300

    dando id pro elemento mosca e verificando se ele ja esta na tela para gerar outro elemento em uma posicao aleatoria, criado tambem dinamica com setInterval para que um novo elemento aparece a cada 1s ou 1000ms

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
+//definindo variaveis globais
 var altura = 0
 var largura = 0
 
+//criando funcao para ajustar tamanho da tela de acao
 function ajustaTamanhoPalcoJogo(){
 
     altura = window.innerHeight
     largura = window.innerWidth
     console.log(largura, altura)
 }
+// chamando alteracao da variavel global altura/largura para as proximas funcoes
 ajustaTamanhoPalcoJogo()
 
-
+// criando funcao para fazer a posicao do elemento ser aleatoria.
+// atribuindo tambem outras funcoes como a de classes e tamanho
 function posicaoRandomica(){
+
+    //remover mosca anterior caso exista
+    if(document.getElementById('mosca')){
+        document.getElementById('mosca').remove()
+    }
+    
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
     
@@ -23,6 +34,7 @@ function posicaoRandomica(){
     var mosca = document.createElement('img')
     mosca.src = 'img/mosca.png'
     mosca.className = (`${tamanhoAleatorio()} ${ladoAleatorio()}`)
+    mosca.id = 'mosca'
     mosca.style.position = 'absolute'
     mosca.style.left = posicaoX + 'px'
     mosca.style.top = posicaoY + 'px'
@@ -31,8 +43,15 @@ function posicaoRandomica(){
 
     tamanhoAleatorio()
 }
-posicaoRandomica()
 
+//Chamando elemento mosca na tela 
+
+
+setInterval(function(){
+    posicaoRandomica()
+}, 1000)
+
+// funcao para aleatorizar tamanho do elemento mosca
 function tamanhoAleatorio(){
     var classe = Math.floor(Math.random() * 3)
     switch (classe){
@@ -47,6 +66,7 @@ function tamanhoAleatorio(){
     }
 }
 
+// funcao para mudar o lado do elemento e dar dinamismo na aplicacao
 function ladoAleatorio(){
     var classe = Math.floor(Math.random() * 2)
     switch (classe){
